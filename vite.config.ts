7aa688def798668a1acd1303d9bffae1df2e1844
lanye74 <layne.ogden727@gmail.com>
2024-08-27 16:12:51 -0400

    fix pwa with @vite-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
 import {sveltekit} from "@sveltejs/kit/vite";
 import {defineConfig} from "vite";
+import {SvelteKitPWA} from "@vite-pwa/sveltekit";
 
 import {readFileSync} from "fs";
+import path from "path";
+
+
+
+const manifestPath = path.join(__dirname, "static/manifest.json");
+const pwaManifest = JSON.parse(readFileSync(manifestPath, {encoding: "utf8"}))!;
 
 
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		// @ts-ignore
+		SvelteKitPWA({...pwaManifest})
+	],
 
 	preview: {
 		https: {
