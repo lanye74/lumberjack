87668b78732ca61e7a0050d55146befebcb25c67
lanye74 <layne.ogden727@gmail.com>
2024-08-27 15:02:41 -0400

    fix https

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import {sveltekit} from "@sveltejs/kit/vite";
 import {defineConfig} from "vite";
 
+import {readFileSync} from "fs";
+
 
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+
+	preview: {
+		https: {
+			key: readFileSync("C:\\Certbot\\live\\appdev.jessamine.kyschools.us\\privkey.pem"),
+			cert: readFileSync("C:\\Certbot\\live\\appdev.jessamine.kyschools.us\\fullchain.pem"),
+		},
+
+		proxy: {} // this is needed for some ungodly reason but it fixes my headaches
+	}
 });
